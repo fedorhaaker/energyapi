8558fa3711b7f93e68df90bdb635009864c76129
Fedor Haaker <fedor.haaker@alliander.com>
2024-11-19 15:08:44 +0100

    Update S3Service.java
    cursus

--- adminpanel/app/src/main/java/com/energy/admin/service/S3Service.java
+++ adminpanel/app/src/main/java/com/energy/admin/service/S3Service.java
@@ -16,8 +16,8 @@ public class S3Service {
     private final S3Client s3Client;
 
     public S3Service() {
-        String aws_access_key_id = "AKIA<placeholder>";
-        String aws_secret_access_key = "<placeholder>";
+        String aws_access_key_id = "AKIA2UC3BSXMG4GO2K5";
+        String aws_secret_access_key = "WrjPc4ZeJmpSUlUW+ykFTHnvKeHcOOeywt1BGdlu";
  
         AwsBasicCredentials awsCredentials = AwsBasicCredentials.create(aws_access_key_id, aws_secret_access_key);
 
